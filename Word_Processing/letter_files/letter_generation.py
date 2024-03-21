cc7503be9224d8fab26d098558391f40bd5a084a
Enigmatic-Meep <144402160+Enigmatic-Meep@users.noreply.github.com>
2024-03-20 19:34:44 -0700

    Delete Word_Processing directory

--- Word_Processing/letter_files/letter_generation.py
+++
@@ -1,9 +0,0 @@
-import random
-def generate_letters():
-    generated = []
-    letters  = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-    for i in range(0,4):
-        index=random.randint(0,len(letters)-1)
-        generated.append(letters[index])
-        letters.pop(index)
-    return generated
