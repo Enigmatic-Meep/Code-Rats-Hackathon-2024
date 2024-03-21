c9d709082ca2fe67c51711f742cb07f65882100d
Enigmatic-Meep <144402160+Enigmatic-Meep@users.noreply.github.com>
2024-03-20 19:38:44 -0700

    Add files via upload

---
+++ GUI/Word_Processing/Scoring.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+import sys
+import os
+
+def assign_points(entry,letters):
+    word = entry.lower()
+    score = 0
+    for letter in letters:
+        if(letter in word):
+            file_name = letter+"_f"
+            file = file_name+".txt"
+
+            here = Path(__file__).resolve()
+            root_folder = here.parents[0]
+            name = "letter_files/"+file
+            my_path = root_folder / name
+  
+            with open(my_path,'r') as f:
+                if(word in f.read()):
+                    score+=1
+            f.close()
+    return score
