cc63aae8df4cdb774c9a7a1f199385a08599537e
Enigmatic-Meep <144402160+Enigmatic-Meep@users.noreply.github.com>
2024-03-20 17:43:48 -0700

    Add files via upload

--- Word_Processing/letter_generation.py
+++ Word_Processing/letter_generation.py
@@ -1,4 +1,5 @@
 import random
+
 def generate_letters():
     generated = []
     letters  = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
